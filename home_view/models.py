ac1b51d7e580b43acfbc4d53898ff0c9fa820f3a
Deathkon <shyakasmith@gmail.com>
2023-11-06 10:30:01 +0100

    first commit

--- home_view/models.py
+++ home_view/models.py
@@ -1,17 +1,3 @@
 from django.db import models
 
-
 # Create your models here.
-
-class Contact(models.Model):
-    full_name = models.CharField(max_length=100)
-    subject = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=100)
-    message = models.TextField()
-
-    def __str__(self):
-        return f'{self.full_name} --- {self.email}'
-
-    class Meta:
-        verbose_name_plural = 'Contact'
